624aad0031978c31fd7d5900ea0ba670f3cfe911
Steven Smith <stevensmith@Stevens-MacBook-Air.local>
2015-05-04 18:51:43 -0600

    We now have a proper structure for our game data, and a proper loop for reading it.

--- python-adventure.py
+++ python-adventure.py
@@ -1,38 +1,42 @@
-player_name = ''
-
 player_name = raw_input("What's your name? ")
 
 game_data = {"one-room house" : {"description" : "You're name is " + player_name + ". You're darkly lit one-room house. Its raining outside. You can hear the drops hit the ceiling and can see rain hit the window when lightning strikes in the distance, which it often does. The window is above a sink which is full of dirty dishes. On the oven beside the sink there is a pot full of boiling water. To your left there is a couch facing a television. The power's out so it's not on. Amazingly there's a penguin sitting on the couch, but they've turned to face you. Behind you is a pile of smelly blankets and an old set of golf clubs.",
                                   "choices" : [{"input" : "t",
-                                               "description" : "talk to the penguin"}]},
-"penguin conversation" : {"description" : "You say, \"Hello penguin.\"\n\n\"Hello " + player_name + ",\" replies the penguin."}}
+                                               "description" : "talk to the penguin",
+                                               "destination" : "penguin conversation"},
+                                               {"input" : "l",
+                                                "description" : "look around the room again",
+                                                "destination" : "one-room house"}]},
+"penguin conversation" : {"description" : "You say, \"Hello penguin.\"\n\n\"Hello " + player_name + ",\" replies the penguin.",
+                          "choices" : [{"input" : "t",
+                                        "description" : "talk to the penguin",
+                                        "destination" : "penguin conversation"},
+                                       {"input" : "l",
+                                        "description" : "look around the room again",
+                                        "destination" : "one-room house"}]}}
 
-print ""
 
-current_room = game_data['one-room house']
-choices = current_room['choices']
+current_room = game_data["one-room house"]
 
-print current_room['description']
+player_input = ""
+while player_input != "q":
+    print "\n" + current_room["description"] + "\n"
 
-player_input = ''
-while player_input != 'q':
-    print ""
-
-    choice = choices[0]['input']
-    description = choices[0]['description']
-
-    print choice + ") " + description
-    print "q) quit game"
-
-    print ""
+    for choice in current_room["choices"]:
+        print choice["input"] + ") " + choice["description"]
+    print "q) quit game\n"
 
     player_input = raw_input("What will you do? ")
 
-    print ""
-
-    if player_input == 't':
-        print game_data['penguin conversation']['description']
-    elif player_input == 'q':
+    if player_input == "q":
         print "Hope you had fun!"
     else:
-        print "I have no idea what you're talking about."
+        valid_choice = False
+
+        for choice in current_room["choices"]:
+            if player_input == choice["input"]:
+                current_room = game_data[choice["destination"]]
+                valid_choice = True
+
+        if not valid_choice:
+            print "I have no idea what you're talking about."
