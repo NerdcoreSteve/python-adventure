f94d942d118d321f954504950c5c984de2d274d8
Steven Smith <stevensmith@Stevens-Air.netgear.com>
2015-06-13 17:48:09 -0600

    added simple templating

--- python-adventure.py
+++ python-adventure.py
@@ -1,16 +1,19 @@
-#make a trello board with stuff from post 4
+import re
+
 #file types should be .game and .saved_game?
 
 player_name = raw_input("What's your name? ")
 
-game_data = {"one-room house" : {"description" : "You're name is " + player_name + ". You're in a darkly lit one-room house. Its raining outside. You can hear the drops hit the ceiling and can see rain hit the window when lightning strikes in the distance, which it often does. The window is above a sink full of dirty dishes. On the oven beside the sink there is a pot full of boiling water. To your left there is a couch facing a television. It's turned to a channel that only gets static. Amazingly there's a penguin sitting on the couch. The penguin turns to face you when you look at it. Behind you is a pile of smelly blankets and an old set of golf clubs.",
+saved_data = {"player name" : player_name}
+
+game_data = {"one-room house" : {"description" : "You're name is {{player name}}. You're in a darkly lit one-room house. Its raining outside. You can hear the drops hit the ceiling and can see rain hit the window when lightning strikes in the distance, which it often does. The window is above a sink full of dirty dishes. On the oven beside the sink there is a pot full of boiling water. To your left there is a couch facing a television. It's turned to a channel that only gets static. Amazingly there's a penguin sitting on the couch. The penguin turns to face you when you look at it. Behind you is a pile of smelly blankets and an old set of golf clubs.",
                                   "choices" : [{"input" : "t",
                                                "description" : "Talk to the penguin",
                                                "destination" : "penguin conversation"},
                                                {"input" : "l",
                                                 "description" : "Look around the room again",
                                                 "destination" : "one-room house"}]},
-"penguin conversation" : {"description" : "You say, \"Hello penguin.\"\n\n\"Hello " + player_name + ",\" replies the penguin.",
+"penguin conversation" : {"description" : "You say, \"Hello penguin.\"\n\n\"Hello {{player name}},\" replies the penguin.",
                           "choices" : [{"input" : "l",
                                         "description" : "look around the room again",
                                         "destination" : "one-room house"},
@@ -32,14 +35,30 @@ game_data = {"one-room house" : {"description" : "You're name is " + player_name
                                         "description" : "look around the room again",
                                         "destination" : "one-room house"}]}}
 
+def add_saved_data(string):
+    matches = re.search("\{\{(.*?)\}\}", string)
+    if(matches):
+        default = matches.group(1)
+        return add_saved_data(
+                re.sub(
+                    matches.group(0),
+                    saved_data[default] if default in saved_data else default,
+                    string))
+    else:
+        return string
+
 current_room = game_data["one-room house"]
 
 player_input = ""
 while player_input != "q":
-    print "\n" + current_room["description"] + "\n"
+    print "\n" + add_saved_data(current_room["description"]) + "\n"
+
+    #if "actions" in current_room:
+    #    perform_actions(current_room["actions"])
+    #make this a dictionary of functions, naming a character is one, battle is another
 
     for choice in current_room["choices"]:
-        print choice["input"] + ") " + choice["description"]
+        print choice["input"] + ") " + add_saved_data(choice["description"])
     print "q) quit game\n"
 
     player_input = raw_input("What will you do? ")
