130279698a4d59ae4ddce5aa2508a2f31e395bab
Steven Smith <stevensmith@Stevens-Air.netgear.com>
2015-10-25 14:05:39 -0600

    loading from .game file

--- python-adventure.py
+++ python-adventure.py
@@ -1,4 +1,5 @@
 import re
+import json
 
 #TODO should grab this from a .save file
 saved_data = {}
@@ -23,81 +24,8 @@ actions['name_entity'](saved_data, {"prompt" : "What's your name?", "label" : "p
 #     Then you won't need this call to name_entity up here, you start with a 'room' that introduces
 #     the game and then goes to the one-room-house
 
-#TODO should grab this from a .game file
-game_data = \
-    {
-        "one-room house":
-            {
-                "description": "Your name is {{player name}}. You're in a darkly lit one-room house. Its raining outside. You can hear the drops hit the ceiling and can see rain hit the window when lightning strikes in the distance, which it often does. The window is above a sink full of dirty dishes. On the oven beside the sink there is a pot full of boiling water. To your left there is a couch facing a television. It's turned to a channel that only gets static. Amazingly there's a penguin sitting on the couch. The penguin turns to face you when you look at it. Behind you is a pile of smelly blankets and an old set of golf clubs.",
-                "choices":
-                    [
-                        {
-                            "input": "t",
-                            "description": "Talk to the penguin",
-                            "destination": "penguin conversation"
-                        },
-                        {
-                            "input": "l",
-                            "description": "Look around the room again",
-                            "destination": "one-room house"
-                        }
-                    ]
-            },
-        "penguin conversation":
-            {
-                "description":
-                    "You say, \"Hello penguin.\"\n\n\"Hello {{player name}},\" replies the penguin.",
-                "choices":
-                    [
-                        {
-                            "input": "l",
-                            "description": "look around the room again",
-                            "destination": "one-room house"
-                        },
-                        {
-                            "input": "a",
-                            "description": "Ask the penguin what's going on.",
-                            "destination": "penguin says what's up"
-                        },
-                        {
-                            "input": "n",
-                            "description": "Ask the penguin their name.",
-                            "destination": "name the penguin"
-                        }
-                    ]
-            },
-        "penguin says what's up":
-            {
-                "description":
-                    "You say, \"What's going on?\"\n\n\"I heard a loud bang outside. I think someone's out there.\" replies the penguin.\n\nJust then, you hear a loud noise. Some thing or some one just hit the wall with a loud thud.",
-                "choices":
-                    [
-                        {
-                            "input": "l",
-                            "description": "look around the room again",
-                            "destination": "one-room house"
-                        },
-                        {
-                            "input": "n",
-                            "description": "Ask the penguin their name.",
-                            "destination": "name the penguin"
-                        }
-                    ]
-            },
-        "name the penguin":
-            {
-                "description":
-                    "You say, \"What's your name?\"\n\nThe penguin says, \"You remember it's...\"",
-                "choices":
-                    [
-                        {
-                            "input" : "l",
-                            "description" : "look around the room again",
-                            "destination" : "one-room house"
-                        }
-                    ]
-            }
-    }
+with open('python-adventure.game') as game_file:    
+    game_data = json.load(game_file)
 
 def add_saved_data(string):
     matches = re.search("\{\{(.*?)\}\}", string)
