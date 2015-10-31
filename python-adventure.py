9b5cc9e46df66c497a29e4353926b1b6fc6fa9ee
Steven Smith <stevensmith@Stevens-Air.netgear.com>
2015-10-31 02:53:57 -0600

    started adding conditions in descriptions

--- python-adventure.py
+++ python-adventure.py
@@ -1,37 +1,21 @@
 import re
 import json
 
-#TODO should grab this from a .save file
-saved_data = {}
-
 def name_entity(saved_data, options):
     saved_data[options['label']] = raw_input(options['prompt'] + " ")
 
-# TODO maybe this should say something like {{player name}} got a {{item}}! or got another {{item}}!
-# TODO I'll need a way for the environment to keep track of how many items there are
-#      this function will probably have to account for that.
 def add_to_inventory(saved_data, options):
     if options['item'] in saved_data['inventory']:
         saved_data['inventory'][options['item']] += 1
     else:
         saved_data['inventory'][options['item']] = 1
 
-actions = {'name_entity' : name_entity, 'add_to_inventory' : add_to_inventory}
-
-actions['name_entity'](saved_data, {"prompt" : "What's your name?", "label" : "player name"})
-
-#TODO make choice have an optional action, if there is an action, call one of the actions functions.
-#     Then you won't need this call to name_entity up here, you start with a 'room' that introduces
-#     the game and then goes to the one-room-house
-
-with open('python-adventure.game') as game_file:    
-    game_data = json.load(game_file)
-
-def add_saved_data(string):
+def add_saved_data(saved_data, string):
     matches = re.search("\{\{(.*?)\}\}", string)
     if(matches):
         default = matches.group(1)
         return add_saved_data(
+                saved_data,
                 re.sub(
                     matches.group(0),
                     saved_data[default] if default in saved_data else default,
@@ -39,16 +23,36 @@ def add_saved_data(string):
     else:
         return string
 
-#TODO .game file should tell us this
-current_room = game_data["one-room house"]
+def apply_conditions(saved_data, string):
+    matches = re.search("\(\((.*?) \? (.*?) \| (.*?)\)\)", string)
+    #if(matches):
+        #print matches.group(1)
+        #print matches.group(2)
+        #print matches.group(3)
+    #else:
+        #print "no matches!"
+    return string
+
+actions = {'name_entity' : name_entity, 'add_to_inventory' : add_to_inventory}
+
+#TODO should grab this from a .save file
+saved_data = {}
+
+actions['name_entity'](saved_data, {"prompt" : "What's your name?", "label" : "player name"})
+
+with open('python-adventure.game') as game_file:    
+    game_data = json.load(game_file)
+
+scenes = game_data['scenes']
+
+current_room = scenes["one-room house"]
 
 player_input = ""
 while player_input != "q":
-    print "\n" + add_saved_data(current_room["description"]) + "\n"
+    print "\n" + apply_conditions(saved_data, add_saved_data(saved_data, current_room["description"])) + "\n"
 
-    #TODO game data should be able to specify a set of default choices you usually always have.
     for choice in current_room["choices"]:
-        print choice["input"] + ") " + add_saved_data(choice["description"])
+        print choice["input"] + ") " + add_saved_data(saved_data, choice["description"])
     print "q) quit game\n"
 
     player_input = raw_input("What will you do? ")
@@ -62,7 +66,7 @@ while player_input != "q":
             if player_input == choice["input"]:
                 if "action" in choice and choice["action"]["name"] in actions:
                     actions[choice["action"]["name"]](saved_data, choice["action"]["options"])
-                current_room = game_data[choice["destination"]]
+                current_room = scenes[choice["destination"]]
                 valid_choice = True
 
         if not valid_choice:
